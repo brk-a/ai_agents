bce13a450a830654fe91823755c63bf87764dbc6
brk-a <fnjakai@outlook.com>
2025-10-13 09:49:59 +0300

    bigram LM

---
+++ LLMs/bigram.py
@@ -0,0 +1,26 @@
+"""
+Class BLM (BigramLanguageModel)
+"""
+
+class BigramLanguageModel:
+    """ class BigramLanguageModel """
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def open_file(self, path_to_file: str):
+        try:
+            with open() as f:
+                pass
+        except TypeError:
+            pass
+        except ValueError:
+            pass
+        else:
+            # raise if other types of error
+            pass
